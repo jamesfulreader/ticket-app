db00a24dc49b5cfd5b1e3156f3432121e968066d
jamesfulreader <jamesfulreader@gmail.com>
2022-12-26 22:04:22 -0700

    create .env file update gitignore file

--- db.js
+++ db.js
@@ -1,3 +1,18 @@
 const { Client } = require('pg')
 const dotenv = require('dotenv')
 dotenv.config()
+
+const connectDb = async () => {
+	try {
+		const client = new Client({
+			user: process.env.PGUSER,
+			host: process.env.PGHOST,
+			database: process.env.PGDATABASE,
+			password: process.env.PGPASSWORD,
+			port: process.env.PGPORT,
+		})
+		await client.connect()
+	} catch (error) {
+		console.log(error)
+	}
+}
