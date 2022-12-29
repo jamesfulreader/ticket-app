d55776204b3c2f3398b339f40cc2024c84b668dc
jamesfulreader <jamesfulreader@gmail.com>
2022-12-28 19:29:20 -0700

    Add put delete and get one ticket request

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/tickets', async (req, res) => {
 	})
 })
 // Get one ticket
-
+app.get('/tickets/:id', async (req, res) => {})
 // Create one ticket
 app.post('/tickets', async (req, res) => {
 	try {
@@ -28,7 +28,9 @@ app.post('/tickets', async (req, res) => {
 	}
 })
 // Update ticket
+app.put('/tickets/:id', async (req, res) => {})
 
 // Delete ticket
+app.delete('/ticket/:id', async (req, res) => {})
 
 app.listen(5001, () => console.log('Sever started on port 5001'))
