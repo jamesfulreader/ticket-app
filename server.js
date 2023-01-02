69eef5fabe1f994d9628a550f46696951920f7c3
jamesfulreader <jamesfulreader@gmail.com>
2023-01-01 20:06:31 -0700

    Add update get one delete routes

--- server.js
+++ server.js
@@ -21,7 +21,20 @@ app.get('/tickets', async (req, res) => {
 	)
 })
 // Get one ticket
-app.get('/tickets/:id', async (req, res) => {})
+app.get('/tickets/:id', async (req, res) => {
+	const id = parseInt(req.params.id)
+
+	pool.query(
+		'SELECT * FROM ticket_item WHERE ticket_id = $1',
+		[id],
+		(err, results) => {
+			if (err) {
+				throw err
+			}
+			res.status(200).json(results.rows)
+		}
+	)
+})
 
 // Create one ticket
 app.post('/tickets', async (req, res) => {
@@ -40,9 +53,37 @@ app.post('/tickets', async (req, res) => {
 	}
 })
 // Update ticket
-app.put('/tickets/:id', async (req, res) => {})
+app.put('/tickets/:id', async (req, res) => {
+	const id = parseInt(req.params.id)
+	const { assignee, priority, status, description, ticket_id } = req.body
+
+	pool.query(
+		'UPDATE ticket_item assignee = $1, priority = $2, status = $3, description = $4 WHERE ticket_id = $5',
+		[assignee, priority, status, description, ticket_id],
+		(error, results) => {
+			if (error) {
+				throw error
+			}
+
+			res.status(200).json({ msg: `Ticket updated with ID ${id}` })
+		}
+	)
+})
 
 // Delete ticket
-app.delete('/ticket/:id', async (req, res) => {})
+app.delete('/ticket/:id', async (req, res) => {
+	const id = parseInt(req.params.id)
+
+	pool.query(
+		'DELETE FROM ticket_item WHERE ticket_id = $1',
+		[id],
+		(error, results) => {
+			if (error) {
+				throw error
+			}
+			res.status(200).json({ msg: `Ticket deleted with ID ${id}` })
+		}
+	)
+})
 
 app.listen(5001, () => console.log('Sever started on port 5001'))
