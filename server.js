2ece38143a1d61be9acb1f3d421e0a76a44971dc
jamesfulreader <jamesfulreader@gmail.com>
2023-01-01 20:56:21 -0700

    test routes

--- server.js
+++ server.js
@@ -55,23 +55,23 @@ app.post('/tickets', async (req, res) => {
 // Update ticket
 app.put('/tickets/:id', async (req, res) => {
 	const id = parseInt(req.params.id)
-	const { assignee, priority, status, description, ticket_id } = req.body
+	const { assignee, priority, status, description } = req.body
 
 	pool.query(
-		'UPDATE ticket_item assignee = $1, priority = $2, status = $3, description = $4 WHERE ticket_id = $5',
-		[assignee, priority, status, description, ticket_id],
+		'UPDATE ticket_item SET assignee = $1, priority = $2, status = $3, description = $4 WHERE ticket_id = $5',
+		[assignee, priority, status, description, id],
 		(error, results) => {
 			if (error) {
 				throw error
 			}
 
-			res.status(200).json({ msg: `Ticket updated with ID ${id}` })
+			res.status(200).json({ msg: `Ticket updated with ID: ${id}` })
 		}
 	)
 })
 
 // Delete ticket
-app.delete('/ticket/:id', async (req, res) => {
+app.delete('/tickets/:id', async (req, res) => {
 	const id = parseInt(req.params.id)
 
 	pool.query(
