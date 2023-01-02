805d7b139df5302df7815db6905cf127bba74878
jamesfulreader <jamesfulreader@gmail.com>
2023-01-01 17:38:05 -0700

    complete get tickets and create ticket routes

--- server.js
+++ server.js
@@ -10,19 +10,31 @@ app.use(express.json())
 // ROUTES
 // Get all tickets
 app.get('/tickets', async (req, res) => {
-	pool.query('SELECT * FROM ticket_item', (err, results) => {
-		if (err) {
-			throw err
+	pool.query(
+		'SELECT * FROM ticket_item ORDER BY ticket_id ASC',
+		(err, results) => {
+			if (err) {
+				throw err
+			}
+			res.status(200).json(results.rows)
 		}
-		console.log(results.rows)
-	})
+	)
 })
 // Get one ticket
 app.get('/tickets/:id', async (req, res) => {})
+
 // Create one ticket
 app.post('/tickets', async (req, res) => {
 	try {
-		console.log(req.body)
+		const { assignee, priority, status, description } = req.body
+		// 1999-01-08 04:05:06 -8:00
+		const timestamp = new Date().toLocaleString('en-GB', { timeZone: 'UTC' })
+
+		const newTicket = await pool.query(
+			'INSERT INTO ticket_item (assignee, timestamp, priority, status, description) VALUES($1, $2, $3, $4, $5)',
+			[assignee, timestamp, priority, status, description]
+		)
+		res.json(newTicket)
 	} catch (error) {
 		console.error(error)
 	}
